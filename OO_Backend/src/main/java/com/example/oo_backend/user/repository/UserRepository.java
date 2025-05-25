db9619c9a583474d0b83eb6a6530384615dbcef4
2jw0305 <ljw03053064@gmail.com>
2025-05-25 15:30:28 +0900

    중복확인 부분들, 탈퇴 기능 완료

--- OO_Backend/src/main/java/com/example/oo_backend/user/repository/UserRepository.java
+++ OO_Backend/src/main/java/com/example/oo_backend/user/repository/UserRepository.java
@@ -8,7 +8,14 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+    // 이메일 중복 확인
     boolean existsByEmail(String email);
+
+    // 휴대폰 번호 중복 확인
+    boolean existsByPhone(String phone);
+
+    // 닉네임(아이디) 중복 확인
+    boolean existsByName(String name);
 
     Optional<User> findByEmail(String email);
 }
