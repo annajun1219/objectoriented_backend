f02457e08ec0dd71231a91d5d21b6cd1395f5894
2jw0305 <ljw03053064@gmail.com>
2025-06-18 06:16:23 +0900

    책 오류 수정

--- OO_Backend/src/main/java/com/example/oo_backend/book/controller/BookController.java
+++ OO_Backend/src/main/java/com/example/oo_backend/book/controller/BookController.java
@@ -70,4 +70,9 @@ public class BookController {
         List<BookPreviewDto> results = bookService.searchByProfessor(keyword);
         return ResponseEntity.ok(results);
     }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<BookPreviewDto>> getAllBooks() {
+        return ResponseEntity.ok(bookService.getAllBooks());
+    }
 }
